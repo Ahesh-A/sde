bef285524099f94e41871fe9d92dd1cf2a297008
Ahesh-A <aheshalagu@gmail.com>
2024-11-19 18:42:33 +0530

    added class LeftRotateByK

--- java/LeftRotateByK.java
+++ java/LeftRotateByK.java
@@ -9,17 +9,16 @@ public class LeftRotateByK {
 		}
 	}
 	public static void leftRotateByK(int[] arr, int k) {
-		reverse(arr, 0, arr.length - 1);
-		System.out.println(Arrays.toString(arr));
-		reverse(arr, 0, k - 1);
-		System.out.println(Arrays.toString(arr));
-		reverse(arr, k, arr.length - 1);
-		System.out.println(Arrays.toString(arr));
+		int n = arr.length;
+		k = k % n;
+		reverse(arr, 0, n - 1);
+		reverse(arr, n - k , n - 1);
+		reverse(arr, 0, n - k - 1);
 	}
 	public static void main(String[] args) {
 		int[] arr = {1, 2, 3, 4, 5, 6};
-		int k = 2;
+		int k = 14;
 		leftRotateByK(arr, k);
-//		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr));
 	}
 }
