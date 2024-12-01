e14630b0f2de23d83b3def240469869fc1a6b163
Ahesh-A <aheshalagu@gmail.com>
2024-12-02 00:00:39 +0530

    added file PrintNTo1.java

---
+++ java/recursion/PrintNTo1.java
@@ -0,0 +1,14 @@
+public class PrintNTo1 {
+	public static void printNTo1(int n) {
+		if(n == 0) {
+			return;
+		}
+
+
+		System.out.println(n);
+		printNTo1(n - 1);
+	}
+	public static void main(String[] args) {
+		printNTo1(8);
+	}
+}
